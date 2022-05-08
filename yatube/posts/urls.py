1c33714d092e7adbd419b0bf9e26f767b2150a16
Buyy <Buyyfly@gmail.com>
2022-05-08 13:56:00 +0300

    ver 1.0

---
+++ yatube/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'index'
+
+urlpatterns = [
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('', views.index, name='post_list'),
+]
